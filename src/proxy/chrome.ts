0b0219c3ee80db535471f9ee85057e05e5574bf8
async-rustacean <swnblovego@gmail.com>
2023-06-07 11:16:32 +0800

    feat(chrome)

---
+++ src/proxy/chrome.ts
@@ -0,0 +1,68 @@
+import type { UnionEventHandler, GetAddEventListenerKeys } from './common'
+import type { ISyncEvent } from '../types'
+import { SyncEvent } from '../sync-event'
+
+type WindowEventHandler = UnionEventHandler<Window, GetAddEventListenerKeys<Window>>
+
+export class ChromeExtensionEventProxyAgent
+  implements
+    Omit<ISyncEvent<WindowEventHandler>, 'publisher' | 'dispatch' | 'createPublisher' | 'observer'>
+{
+  // combination better than extends
+  #syncEvent = SyncEvent.new<WindowEventHandler>()
+
+  #removeEvenListenerQueue: (readonly [string, (...args: any[]) => void])[] = []
+
+  // rewrite all methods , this is the cost for not use extends
+
+  interceptDispatch = this.#syncEvent.interceptDispatch
+
+  unInterceptDispatch = this.#syncEvent.unInterceptDispatch
+
+  on = this.#syncEvent.on
+
+  offAll = this.#syncEvent.offAll
+
+  off = this.#syncEvent.off
+
+  once = this.#syncEvent.once
+
+  any = this.#syncEvent.any
+
+  waitUtil = this.#syncEvent.waitUtil
+
+  createObserver = this.#syncEvent.createObserver
+
+  constructor() {
+    this.#proxyWindow()
+  }
+
+  static new() {
+    return new ChromeExtensionEventProxyAgent()
+  }
+
+  destroy = () => {
+    this.offAll()
+    this.#removeEvenListenerQueue.forEach(pair => {
+      window.removeEventListener(pair[0], pair[1])
+    })
+  }
+
+  #proxyWindow = () => {
+    Object.keys(window)
+      .filter(key => key.startsWith('on'))
+      .filter(key => window[key] === null || typeof window[key] === 'function')
+      .forEach(key => {
+        const dispatchKey = key.slice(2)
+        const pair = [
+          dispatchKey,
+          (...args: any[]) => {
+            this.#syncEvent.dispatch(dispatchKey as any, ...args)
+          },
+        ] as const
+        window.addEventListener(pair[0], pair[1])
+
+        this.#removeEvenListenerQueue.push(pair)
+      })
+  }
+}
