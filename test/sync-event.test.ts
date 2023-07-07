67eeccec76c6d34b6a361aa8cc38575676c9e836
async-rustacean <swnblovego@gmail.com>
2023-07-07 10:35:20 +0800

    test(): update test

--- test/sync-event.test.ts
+++ test/sync-event.test.ts
@@ -1,4 +1,8 @@
-import { SyncEvent } from '../src'
+/**
+ * @jest-environment jsdom
+ */
+
+import { EventProxy, SyncEvent } from '../src'
 
 // define event handler type
 
@@ -89,4 +93,148 @@ test('test sync event on and once', () => {
   expect(count).toBe(2)
 })
 
-test('test sync event', () => {})
+test('test event proxy', () => {
+  const div = document.createElement('div')
+
+  const divEventProxyAgent = EventProxy.new(div)
+
+  let anyCount = 0
+  divEventProxyAgent.any((evType, ...args) => {
+    expect(evType).toBe('click')
+    anyCount += 1
+  })
+
+  let clickCount = 0
+
+  const cancel = divEventProxyAgent.on('click', () => {
+    clickCount += 1
+  })
+
+  div.click()
+
+  expect(clickCount).toBe(1)
+
+  div.click()
+
+  expect(clickCount).toBe(2)
+
+  cancel()
+
+  div.click()
+
+  expect(clickCount).toBe(2)
+
+  expect(anyCount).toBe(2)
+})
+
+test('test any event proxy', () => {
+  const div = document.createElement('div')
+
+  const divEventProxyAgent = EventProxy.new(div, { proxyAllEvent: true })
+
+  let anyCount = 0
+  divEventProxyAgent.any((ev: any) => {
+    expect(ev).toBe('click')
+    anyCount += 1
+  })
+
+  let clickCount = 0
+
+  const cancel = divEventProxyAgent.on('click', () => {
+    clickCount += 1
+  })
+
+  div.click()
+
+  expect(clickCount).toBe(1)
+
+  div.click()
+
+  expect(clickCount).toBe(2)
+
+  cancel()
+
+  div.click()
+
+  expect(clickCount).toBe(2)
+
+  expect(anyCount).toBe(3)
+})
+
+test('test any event proxy', () => {
+  const div = document.createElement('div')
+
+  const divEventProxyAgent = EventProxy.new(div, { proxyAllEvent: true })
+
+  let anyCount = 0
+  divEventProxyAgent.any((evType, ...args) => {
+    expect(evType).toBe('click')
+    anyCount += 1
+  })
+
+  let clickCount = 0
+
+  const cancel = divEventProxyAgent.on('click', () => {
+    clickCount += 1
+  })
+
+  div.click()
+
+  expect(clickCount).toBe(1)
+
+  div.click()
+
+  expect(clickCount).toBe(2)
+
+  cancel()
+
+  div.click()
+
+  expect(clickCount).toBe(2)
+
+  expect(anyCount).toBe(3)
+})
+
+test('test sync event waitUtil', async () => {
+  const div = document.createElement('div')
+
+  const divEventProxyAgent = EventProxy.new(div)
+  for (let i = 0; i < 3; i++) {
+    await Promise.all([
+      divEventProxyAgent.waitUtil('click', {
+        timeout: 1000,
+        where(ev) {
+          expect(ev.type).toBe('click')
+          return ev.type === 'click'
+        },
+      }),
+      new Promise<void>(res => {
+        res()
+        div.click()
+      }),
+    ])
+  }
+})
+
+test('test sync event bind for window', async () => {
+  const windowEventProxyAgent = EventProxy.new(window)
+
+  await Promise.all([
+    windowEventProxyAgent.waitUtil('resize', {
+      timeout: 2000,
+      where(ev) {
+        expect(ev.type).toBe('resize')
+        return window.innerWidth === 191
+      },
+    }),
+    (async () => {
+      for (let i = 0; i < 2; i++) {
+        global.innerWidth = 190 + i
+        global.dispatchEvent(new Event('resize'))
+        await new Promise<void>(r => {
+          setTimeout(r, 1000)
+        })
+      }
+    })(),
+  ])
+})
