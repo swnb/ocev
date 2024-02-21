5473aae5b4fc5ced207efd6790cef7f0be036f56
async-rustacean <swnblovego@gmail.com>
2024-02-21 13:47:38 +0800

    chore(type): update type

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,10 @@ export type HandlerMap = Record<string, (...arg: any) => void | Promise<void>>
 export type CancelRef = { current: () => void }
 
 export type WaitUtilConfig<Args extends any[]> = {
-  timeout?: number
-  cancelRef?: CancelRef
-  where?: (...args: Args) => boolean
-  mapToError?: (...args: Args) => any
+  timeout?: number // Optional timeout for the waitUtil method.
+  cancelRef?: CancelRef // Reference to a cancellation function to avoid memory leaks.
+  where?: (...args: Args) => boolean // Optional condition function to filter events.
+  mapToError?: (...args: Args) => any // Optional function to map event arguments to an error.
 }
 
 export type EventListItem<M extends HandlerMap, K extends keyof M> = K extends keyof M
@@ -17,71 +17,188 @@ export type EventListItem<M extends HandlerMap, K extends keyof M> = K extends k
 
 export interface ISyncEvent<M extends HandlerMap> {
   /**
-   * observer only allow to call method : 'on' | 'once' | 'off' | 'waitUtil'
+   * Get listener count by event name
+   * If no event name is passed, returns the total number of listeners for all events.
+   * @param {K} event eventName
+   * @returns {number} listener count
    */
-  readonly observer: Pick<this, 'on' | 'once' | 'off' | 'waitUtil'>
+  listenerCount: <K extends keyof M>(event?: K | undefined) => number
+
   /**
-   * publisher only allow to call method : 'emit' | 'interceptEmit' | 'unInterceptEmit'
+   * Register an event handler for the specified event.
+   * @param {K} event Event name
+   * @param {M[K]} handler Event handler callback function
+   * @param {ListenerOptions} [options] Listener options support debounce and throttle , more details in the document
+   * @returns {LinkableListener<M>} Linkable listener object for chaining
    */
-  readonly publisher: Pick<this, 'emit' | 'interceptEmit' | 'unInterceptEmit'>
+  on: <K extends keyof M>(event: K, handler: M[K], options?: ListenerOptions) => LinkableListener<M>
   /**
-   * interceptEmit will stop all emit
-   * util unInterceptEmit is called
+   * Add a handler which listen to any emitted event.
+   * this return value of any can't be chained
+   * @template K - The type representing keys of the handler map.
+   * @param {Function} handler - The function to be invoked when any event is emitted.
+   * @returns {Function} - A function that, when called, removes the added handler.
    */
-  interceptEmit: VoidFunction
+  any: <K extends keyof M = keyof M>(
+    handler: (event: K, ...args: Arguments<M[K]>) => void,
+  ) => () => void
   /**
-   * interceptEmit will resume all emit
-   * nothing will happen if interceptEmit is not called
-   */
-  unInterceptEmit: VoidFunction
-  /**
+   * Register a handler to be executed only once when the specified event is emitted.
    *
-   * @param type  event type , same as emit event type
-   * @param handler  callback will run when emit same event type
-   * @returns
+   * @template K - The type representing keys of the handler map.
+   * @param {K} event - The event to listen for.
+   * @param {M[K]} handler - The handler function to be executed when the event is emitted.
+   * @returns {LinkableListener<M>} - An object containing methods to manage the listener.
    */
-  on: <K extends keyof M>(type: K, handler: M[K], options?: ListenerOptions) => LinkableListener<M>
+  once: <K extends keyof M>(event: K, handler: M[K]) => LinkableListener<M>
   /**
-   * @param type event type
-   * @param handler  callback only run one time
-   * @returns
+   * Removes all handlers for the specified event or clears all handlers if no event is specified.
+   *
+   * @template K - The type representing keys of the handler map.
+   * @param {K} [event] - The event for which handlers should be removed. If not provided, all handlers are cleared.
+   * @returns {this} - The instance of the event emitter for chaining.
    */
-  once: <K extends keyof M>(type: K, handler: M[K]) => LinkableListener<M>
+  offAll: <K extends keyof M>(event?: K | undefined) => void
   /**
-   * auto clear all callback
-   * @param type
-   * @returns {this}
+   * Removes a specific handler for the specified event.
+   *
+   * @template K - The type representing keys of the handler map.
+   * @param {K} event - The event from which the handler should be removed.
+   * @param {M[K]} handler - The handler function to be removed.
+   * @returns {this} - The instance of the event emitter for chaining.
    */
+  off: <K extends keyof M>(event: K, handler: M[K]) => this
   /**
-   * @param handler  any emit will emit handler
-   * @returns
+   * Emits the specified event with the provided arguments, invoking all registered handlers for the event.
+   *
+   * @template K - The type representing keys of the handler map.
+   * @param {K} event - The event to be emitted.
+   * @param {...Arguments<M[K]>} args - The arguments to be passed to the event handlers.
+   * @returns {this} - The instance of the event emitter for chaining.
    */
-  any: (
-    handler: <T extends keyof M = keyof M>(type: T, ...args: Arguments<M[T]>) => any,
-  ) => VoidFunction
-  offAll: <K extends keyof M>(type?: K | undefined) => this
-  off: <K extends keyof M>(type: K, handler: M[K]) => this
-  emit: <K extends keyof M>(type: K, ...arg: Arguments<M[K]>) => this
-  waitUtil: <K extends keyof M>(
-    type: K,
+  emit: <K extends keyof M>(event: K, ...args: Arguments<M[K]>) => this
+  /**
+   * Returns a promise that resolves when the specified event is emitted.
+   *
+   * @template K - The type representing keys of the handler map.
+   * @param {K} event - The name of the event to wait for.
+   * @param {WaitUtilConfig<Arguments<M[K]>>} [config={}] - Configuration options for the waitUtil method.
+   * @returns {Promise<Arguments<M[K]>>} - A promise that resolves with the event arguments.
+   */
+  waitUtil: <K extends keyof M = keyof M>(
+    event: K,
     config?: WaitUtilConfig<Arguments<M[K]>>,
   ) => Promise<Arguments<M[K]>>
   /**
-   * create Observer with access control that observer can only listen to specify events , and other behavior
-   * @param {ObserverAccessControl}
-   * @returns {Observer}
+   * Waits for all events in the specified list to be emitted.
+   *
+   * @template K - The type representing keys of the handler map.
+   * @template EventList - The type representing the list of events to wait for.
+   * @param {EventList} eventList - The list of events to wait for.
+   * @returns {Promise<ExtractHandlerMapArgumentsFromEventListItem<M, K, EventList>>} - A promise that resolves with the arguments of all emitted events.
    */
-  createObserver: <K extends keyof M>(
-    accessControl?: ObserverAccessControl<K>,
-  ) => IAccessControlObserver<M, K>
+  waitUtilAll: <
+    K extends keyof M = keyof M,
+    EventList extends readonly (K | Readonly<EventListItem<M, K>>)[] = readonly (
+      | K
+      | Readonly<EventListItem<M, K>>
+    )[],
+  >(
+    eventList: EventList,
+  ) => Promise<ExtractHandlerMapArgumentsFromEventListItem<M, K, EventList>>
   /**
-   * create Publisher with access control that can publish specify events , and other behavior
-   * @param {PublisherAccessControl}
-   * @returns {Publisher}
+   * Waits for the first event in the specified list to be emitted.
+   *
+   * @template K - The type representing keys of the handler map.
+   * @param {((EventListItem<M, K> | K)[])} eventList - The list of events to wait for.
+   * @returns {Promise<WaitUtilCommonReturnValue<M, K>>} - A promise that resolves with the result of the first emitted event.
    */
-  createPublisher: <K extends keyof M>(
-    accessControl?: PublisherAccessControl<K>,
-  ) => IAccessControlPublisher<M, K>
+  waitUtilRace: <K extends keyof M = keyof M>(
+    eventList: (K | EventListItem<M, K>)[],
+  ) => Promise<WaitUtilCommonReturnValue<M, K>>
+  /**
+   * Waits for any event in the specified list to be emitted.
+   *
+   * @template K - The type representing keys of the handler map.
+   * @param {((EventListItem<M, K> | K)[])} eventList - The list of events to wait for.
+   * @returns {Promise<WaitUtilCommonReturnValue<M, K>>} - A promise that resolves with the result of the first emitted event from the list.
+   */
+  waitUtilAny: <K extends keyof M = keyof M>(
+    eventList: (K | EventListItem<M, K>)[],
+  ) => Promise<WaitUtilCommonReturnValue<M, K>>
+  /**
+   * Creates a stream producer with support for async iteration.
+   * Strategy will work when strategy.capacity is large than zero
+   * producer will either 'drop' or 'replace' new event when event queue's length is equal to capacity
+   * @param {K[]} eventList - An array of events to subscribe to.
+   * @param {EventStreamStrategy} strategy - The strategy to apply when the stream is full.
+   * @returns {Object} - An object with methods for async iteration and event count information.
+   */
+  createEventStream: <K extends keyof M = keyof M>(
+    eventList: K[],
+    strategy?: EventStreamStrategy,
+  ) => {
+    [Symbol.asyncIterator]: () => {
+      next: () => Promise<{
+        value: WaitUtilCommonReturnValue<M, K>
+        done: boolean
+      }>
+      return: () => Promise<{
+        value: WaitUtilCommonReturnValue<M, K>
+        done: boolean
+      }>
+    }
+    droppedEventCount: () => number
+    replacedEventCount: () => number
+  }
+  /**
+   * Creates a ReadableStream containing the event stream.
+   *
+   * @param {K[]} eventList - An array of events to subscribe to.
+   * @param {EventStreamStrategy} strategy - The strategy to apply when the stream is full.
+   * @returns {ReadableStream} - A ReadableStream containing the event stream.
+   */
+  createEventReadableStream: <K extends keyof M = keyof M>(
+    eventList: K[],
+    strategy?: EventStreamStrategy,
+  ) => ReadableStream<WaitUtilCommonReturnValue<M, K>>
+
+  /**
+     * Observer only allow to call methods below:
+     *| 'on'
+      | 'once'
+      | 'off'
+      | 'any'
+      | 'waitUtil'
+      | 'waitUtilRace'
+      | 'waitUtilAll'
+      | 'waitUtilAny'
+      | 'createEventStream'
+      | 'createEventReadableStream'
+     */
+  get subscriber(): Pick<
+    this,
+    | 'on'
+    | 'once'
+    | 'off'
+    | 'waitUtil'
+    | 'any'
+    | 'waitUtilRace'
+    | 'waitUtilAll'
+    | 'waitUtilAny'
+    | 'createEventStream'
+    | 'createEventReadableStream'
+  >
+
+  /**
+   * Publisher only allow to call method 'emit'
+   */
+  get publisher(): Pick<this, 'emit'>
+  /**
+   * Create new SyncEvent
+   * @returns {SyncEvent<M>} 新的 SyncEvent 实例
+   * @static
+   */
 }
 
 export type Arguments<T extends (...args: any) => any> = T extends (...args: infer R) => void
@@ -104,8 +221,7 @@ export interface IAccessControlObserver<M extends HandlerMap, K extends keyof M>
   on: (type: K, handler: M[K]) => VoidFunction
 }
 
-export interface IAccessControlPublisher<M extends HandlerMap, K extends keyof M>
-  extends Pick<ISyncEvent<M>, 'interceptEmit' | 'unInterceptEmit'> {
+export interface IAccessControlPublisher<M extends HandlerMap, K extends keyof M> {
   emit: (type: K, ...arg: Arguments<M[K]>) => this
 }
 
